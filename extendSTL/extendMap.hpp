035d68d2f490673bae66533610cfb60312ae252d
aurshine <104882342+aurshine@users.noreply.github.com>
2023-07-05 10:34:42 +0800

    Add files via upload

---
+++ extendSTL/extendMap.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <map>
+#include <utility>
+#include <functional>
+
+template <class K, class V>
+bool maxValue(std::pair<K, V> p1, std::pair<K, V> p2)
+{
+	return p1.second > p2.second;
+}
+
+template <class K, class V>
+std::pair<K, V> maxInMap(const std::map<K, V>& mp,
+	std::function<bool(std::pair<K, V> p1, std::pair<K, V> p2)> f = maxValue<K, V>)
+{
+	bool flag = false;
+	std::pair<K, V> maxv{};
+
+	for (auto iter : mp)
+	{
+		if (!flag)
+		{
+			maxv = iter;
+			flag = true;
+		}
+
+		if (!f(maxv, iter))
+			maxv = iter;
+	}
+
+	return maxv;
+}
---
+++ extendSTL/train_test_split.hpp
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include "extendVector.hpp"
+#include <utility>
+#include <random>
+#include <algorithm>
+
+/*
+* return pair<vector, vector>, first is train, second is test
+*
+* vector typed pair<T1, T2>, first is feature, second is target
+*
+*/
+template<class T1, class T2>
+std::pair<std::vector<std::pair<T1, T2>>, std::vector<std::pair<T1, T2>>>
+train_test_split(
+	std::vector<std::pair<T1, T2>>& datas,
+	double trainSize = 0.7
+)
+{
+	/*
+	* provide random seed
+	*/
+	std::random_device rd;
+	/*
+	* visual random engine
+	*/
+	std::mt19937 rng(rd());
+
+	/*
+	* std::shuffle need mutable argument
+	* const std::vector<> will compilation error
+	*/
+	std::shuffle(datas.begin(), datas.end(), rng);
+
+	size_t mid = datas.size() * trainSize;
+
+	return std::make_pair(splitVector<std::pair<T1, T2>>(datas, 0, mid),
+		splitVector<std::pair<T1, T2>>(datas, mid + 1, datas.size() - 1));
+}
+
+/*
+* right value reference
+*/
+template<class T1, class T2>
+std::pair<std::vector<std::pair<T1, T2>>, std::vector<std::pair<T1, T2>>>
+train_test_split(
+	std::vector<std::pair<T1, T2>>&& datas,
+	double trainSize = 0.7
+)
+{
+	/*
+	* provide random seed
+	*/
+	std::random_device rd;
+	/*
+	* visual random engine
+	*/
+	std::mt19937 rng(rd());
+
+	std::shuffle(datas.begin(), datas.end(), rng);
+
+	size_t mid = datas.size() * trainSize;
+
+	return std::make_pair(splitVector<std::pair<T1, T2>>(datas, 0, mid),
+		splitVector<std::pair<T1, T2>>(datas, mid + 1, datas.size() - 1));
+}
